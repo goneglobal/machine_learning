77b859beb11490ba96f10bcf274761646a769d30
Adam Adair <adam@mac.local>
2025-09-22 20:47:10 +1000

    basic sequential tensorflow

---
+++ src/basic-model/basic-model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(32,)))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(tf.random.normal((1000, 32)), 
+          tf.random.uniform((1000,), 
+                            maxval=10, 
+                            dtype=tf.int64), 
+          epochs=10)
+
+model.predict(tf.random.normal((1, 32)))
